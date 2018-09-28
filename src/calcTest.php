be28ddaa4940686704d73215ec580380f2a61240
Andrew Bugakov <31094497+a-westbrook@users.noreply.github.com>
2018-09-28 12:30:54 +0300

    unit tests

--- src/calcTest.php
+++ src/calcTest.php
@@ -1,6 +1,6 @@
 <?php
 // CalculatorTest.php
-include_once("Calculator.php");
+include_once("calc.php");
 class CalculatorTest extends \PHPUnit_Framework_TestCase {
     public function testDivideByPositiveNumber() {
         $calcMock=$this->getMock('\Calculator',array('getNumberFromUserInput'));
